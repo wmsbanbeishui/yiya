9dd4f6d5318defa25d0c075a68772d428f1c0acd
wangmengsi <269243737@qq.com>
2022-10-26 15:58:09 +0800

    save

--- common/helpers/Helper.php
+++ common/helpers/Helper.php
@@ -693,7 +693,7 @@ class Helper
 
         // 接口调试临时加的
         if (isset($_SERVER['HTTP_HOST']) && in_array($_SERVER['HTTP_HOST'], ['47.107.73.157:8083'])) {
-            $img_host = '47.107.73.157:8083';
+            $img_host = '47.107.73.157:8084';
         }
 
         $request_scheme = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http';
