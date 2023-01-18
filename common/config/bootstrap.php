23f39de269989398f09a9a6fbc3f5c82a089dc08
wangmengsi <269243737@qq.com>
2023-01-18 11:04:29 +0800

    获取视频第一帧图片扩展

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,5 +3,6 @@
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@upload', dirname(__DIR__).'/upload');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@capi', dirname(dirname(__DIR__)) . '/capi');
 Yii::setAlias('@admin', dirname(dirname(__DIR__)).'/admin');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
